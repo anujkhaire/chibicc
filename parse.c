f8e57cb07c3f873eda2d0caea7983334806cbf9b
Rui Ueyama <ruiu@cs.stanford.edu>
2020-08-27 22:01:34 +0900

    Add return that doesn't take any value

--- parse.c
+++ parse.c
@@ -1063,7 +1063,7 @@ static bool is_typename(Token *tok) {
   return find_typedef(tok);
 }
 
-// stmt = "return" expr ";"
+// stmt = "return" expr? ";"
 //      | "if" "(" expr ")" stmt ("else" stmt)?
 //      | "switch" "(" expr ")" stmt
 //      | "case" const-expr ":" stmt
@@ -1079,6 +1079,9 @@ static bool is_typename(Token *tok) {
 static Node *stmt(Token **rest, Token *tok) {
   if (equal(tok, "return")) {
     Node *node = new_node(ND_RETURN, tok);
+    if (consume(rest, tok->next, ";"))
+      return node;
+
     Node *exp = expr(&tok, tok->next);
     *rest = skip(tok, ";");
 
