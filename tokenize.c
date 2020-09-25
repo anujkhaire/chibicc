9ead22b204b2e7dec1d325c4e67918e2360ac5c1
Rui Ueyama <ruiu@cs.stanford.edu>
2020-09-26 08:59:56 +0900

    Support single-letter local variables

--- tokenize.c
+++ tokenize.c
@@ -83,6 +83,13 @@ Token *tokenize(char *p) {
       continue;
     }
 
+    // Identifier
+    if ('a' <= *p && *p <= 'z') {
+      cur = cur->next = new_token(TK_IDENT, p, p + 1);
+      p++;
+      continue;
+    }
+
     // Multi-letter punctuators
     if (startswith(p, "==") || startswith(p, "!=") ||
         startswith(p, "<=") || startswith(p, ">=")) {
